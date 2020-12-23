8f596795d1a9e4b0559c3fe531000dc1f045eed0
ruoshui <ruoshui20201101@yeah.net>
2020-12-23 22:51:26 +0800

    frist

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { AxiosRequestConfig } from './types'
+
+function axios(config: AxiosRequestConfig) {
+  // TODO
+  
+}
+
+export default axios
---
+++ src/xhr.ts
@@ -0,0 +1,14 @@
+/*
+* 实现请求方法
+* */
+import { AxiosRequestConfig } from './types'
+
+export default function xhr(config: AxiosRequestConfig): void {
+  const { url, data = null, method = 'get' } = config
+
+  const request = new XMLHttpRequest()
+
+  request.open(method.toUpperCase(), url, true)
+
+  request.send(data)
+}
